f0c081d94faeaf6936e65a2a9dfb5b1d4e69b483
lfpborba <luis@DESKTOP-P2DV0P5.localdomain>
2022-12-14 10:38:08 -0300

    feat: implementing get_unique_job function

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -11,21 +11,9 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    with open(path, 'r') as file:
+        reader = csv.DictReader(file)
+        return list({row['job_type'] for row in reader})
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
