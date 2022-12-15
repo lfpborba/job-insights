aec6adb6d2ea07edc3d3dcc126a688b21b070eda
lfpborba <luisfpb@icloud.com>
2022-12-15 01:51:26 -0300

    feat: implementing test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,19 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "Python") == 1639
+
+
+def test_counter_2():
+    assert count_ocurrences("data/jobs.csv", "Java") == 676
+
+
+def test_counter_3():
+    assert count_ocurrences("data/jobs.csv", "javascript") == 122
+
+
+def test_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        count_ocurrences("data/jobs.json", "javascript")
