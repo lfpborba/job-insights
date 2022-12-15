abab29acbb3f994ff84d5e32d70ecc8efe74b0cd
lfpborba <luisfpb@icloud.com>
2022-12-15 00:40:03 -0300

    feat: implementing get_unique_industries function

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,16 @@
 from typing import List, Dict
 
+import csv
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    with open(path, 'r') as file:
+        reader = csv.DictReader(file)
+        job_list = set()
+        for row in reader:
+            if(row['industry'] == ''):
+                continue
+            job_list.add(row['industry'])
+        return list(job_list)
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
