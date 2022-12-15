5a9db828adcfaefd7ec43f7e4c1575ba3ff64ec7
lfpborba <luisfpb@icloud.com>
2022-12-15 00:42:49 -0300

    fix: industries.py lint

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 
 import csv
 
+
 def get_unique_industries(path: str) -> List[str]:
     with open(path, 'r') as file:
         reader = csv.DictReader(file)
@@ -10,7 +11,7 @@ def get_unique_industries(path: str) -> List[str]:
             if(row['industry'] == ''):
                 continue
             job_list.add(row['industry'])
-        return list(job_list)
+    return list(job_list)
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
