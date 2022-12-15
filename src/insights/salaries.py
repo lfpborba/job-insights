4224cb1026a61233294ba8301384a503f511f291
lfpborba <luisfpb@icloud.com>
2022-12-15 01:33:34 -0300

    feat: implementing get_min_salary function

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -13,21 +13,13 @@ def get_max_salary(path: str) -> int:
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    jobs_list = read(path)
+    min_salaries = [
+        int(job["min_salary"])
+        for job in jobs_list
+        if job["min_salary"].isdigit()
+    ]
+    return min(min_salaries)
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
